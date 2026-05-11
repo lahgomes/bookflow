f54ae526601ea397ebbd2f8aefa189aeb0e75b4b
larissa <lahgomes19@gmail.com>
2026-05-11 14:59:20 -0300

    fix: restore authRoutes registration after books merge

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { corsPlugin } from './plugins/cors.plugin'
 import { swaggerPlugin } from './plugins/swagger.plugin'
 import { jwtPlugin } from './plugins/jwt.plugin'
 import { AppError } from './shared/errors'
+import { authRoutes } from './modules/auth/auth.routes'
 import { booksRoutes } from './modules/books/books.routes'
 
 export function buildApp() {
@@ -19,6 +20,7 @@ export function buildApp() {
   app.register(corsPlugin)
   app.register(swaggerPlugin)
   app.register(jwtPlugin)
+  app.register(authRoutes)
   app.register(booksRoutes)
 
   app.setErrorHandler((rawError, request, reply) => {
